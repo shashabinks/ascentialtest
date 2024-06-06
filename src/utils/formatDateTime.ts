3c314ad8b905d68fa1309da528cdebf861810ca8
shashabinks <shahinshahrokh@rocketmail.com>
2024-06-06 21:32:32 +0100

    added user local time hover

--- src/utils/formatDateTime.ts
+++ src/utils/formatDateTime.ts
@@ -1,11 +1,12 @@
-export function formatDateTime(timestamp: Date) {
-  return new Intl.DateTimeFormat('en-US', {
-    year: 'numeric',
-    month: 'long',
-    day: 'numeric',
-    hour: 'numeric',
-    minute: 'numeric',
-    second: 'numeric',
-    timeZoneName: 'short',
+export function formatDateTime(timestamp: Date, timezone: string) {
+  return new Intl.DateTimeFormat("en-US", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+    second: "numeric",
+    timeZone: timezone,
+    timeZoneName: "short",
   }).format(new Date(timestamp));
 }
